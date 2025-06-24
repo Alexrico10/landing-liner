6a096d851b40fbae4e5e2102693f826101995c27
Alex <alexrico_dev@hotmail.com>
2025-06-24 10:57:07 +0200

    mensaje

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,20 +2,20 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-dark-bg flex px-10 justify-between text-white py-6 border-t border-gray-700">
-      <p className="text-sm">
+    <footer className="w-full bg-dark-bg flex flex-col md:flex-row px-6 md:px-10 justify-between items-center text-white py-6 border-t border-gray-700 gap-2 md:gap-0">
+      <p className="text-sm text-center md:text-left">
         © {new Date().getFullYear()}{" "}
         <Link
-          href={"https://www.instagram.com/liner.tattoo/?igsh=dGxlMmc1YW5qeWt6#"}
+          href="https://www.instagram.com/liner.tattoo/?igsh=dGxlMmc1YW5qeWt6#"
           className="cursor-pointer hover:text-yellow-500"
         >
           LinerTattoo
         </Link>
       </p>
-      <p className="text-sm">
+      <p className="text-sm text-center md:text-right">
         Developed by{" "}
         <Link
-          href={"https://www.instagram.com/alex_rico10/?hl=es"}
+          href="https://www.instagram.com/alex_rico10/?hl=es"
           className="cursor-pointer hover:text-yellow-500"
         >
           Alex Rico
